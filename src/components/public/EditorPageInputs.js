36b7296e77a53696ba4f65d418a0f05880fed1ec
amankashyap004 <amankashyap004@gmail.com>
2023-01-21 20:03:59 +0530

    fix bugs and input value state

--- src/components/public/EditorPageInputs.js
+++ src/components/public/EditorPageInputs.js
@@ -2,10 +2,33 @@ import React from "react";
 import Input from "../../utils/Input";
 
 export default function EditorPageInputs() {
+   const [inputValue, setInputValue] = React.useState({
+      topText: "LET'S MAKE MONEY",
+      bottomText: "TOGETHER",
+   });
+
+   function handleChange(event) {
+      const { name, value } = event.target;
+      setInputValue((prevFromData) => ({ ...prevFromData, [name]: value }));
+   }
+
+   console.log(inputValue.topText);
+   console.log(inputValue.bottomText);
+
    return (
       <section className="px-8 py-4">
-         <Input inputText="Top Text" />
-         <Input inputText="Bottom Text" />
+         <Input
+            inputText="Top Text"
+            name="topText"
+            value={inputValue.topText}
+            onChange={handleChange}
+         />
+         <Input
+            inputText="Bottom Text"
+            name="bottomText"
+            value={inputValue.bottomText}
+            onChange={handleChange}
+         />
       </section>
    );
 }
--- src/components/public/ResultContainer.js
+++ src/components/public/ResultContainer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function ResultContainer() {
    return (
-      <section className="flex justify-center items-center p-4 rounded">
-         <img src="/images/Image.jpg" className="w-full rounded" />
+      <section className="flex justify-center items-center flex-col p-4 rounded relative">
+         <h3 className="absolute top-0 p-8 font-semibold text-lg">Yes this is text</h3>
+         <img src="/images/Image.jpg" className="w-full rounded" alt="post-img" />
+         <h3 className="absolute bottom-0 p-8 font-semibold text-lg">Yes this is text</h3>
       </section>
    );
 }
