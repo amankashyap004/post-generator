7c4e9d6ccfa0621f9b6b57bf7052ec4c86dd41d0
amankashyap004 <amankashyap004@gmail.com>
2023-02-10 17:52:35 +0530

    change background-color

--- src/pages/public/ResultPage.js
+++ src/pages/public/ResultPage.js
@@ -3,7 +3,7 @@ import ResultContainer from "../../components/public/ResultContainer";
 
 export default function ResultPage(props) {
    return (
-      <main className="bg-slate-700 w-1/2 px-8 py-4">
+      <main className="w-1/2 px-8 py-4">
          <div className="flex justify-center items-center">
             <h1 className="text-white text-xl font-bold">Result</h1>
          </div>
