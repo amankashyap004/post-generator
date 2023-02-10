479ce150aa7c5308c8546b1617961d8212607dc2
amankashyap004 <amankashyap004@gmail.com>
2023-02-10 18:31:57 +0530

    delete EditorPageInputs

--- src/pages/public/EditorPage.js
+++ src/pages/public/EditorPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import EditorPageInputs from "../../components/public/EditorPageInputs";
+import DraftEditor from "../../components/public/DraftEditor";
 
 export default function EditorPage(props) {
    return (
@@ -7,9 +8,9 @@ export default function EditorPage(props) {
          <div className="flex justify-center items-center">
             <h1 className="text-white text-xl font-bold">Editor</h1>
          </div>
-         <div>
-            <EditorPageInputs value={props.value} onChange={props.onChange} />
-         </div>
+         <section className="px-8 py-4">
+            <DraftEditor />
+         </section>
       </main>
    );
 }
