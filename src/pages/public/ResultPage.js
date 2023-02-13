691ad4ca2fd766eaef2e213c1419334f8ed2e1af
amankashyap004 <amankashyap004@gmail.com>
2023-02-13 17:38:11 +0530

    add new meme btn

--- src/pages/public/ResultPage.js
+++ src/pages/public/ResultPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ResultContainer from "../../components/public/ResultContainer";
+import SmallBtn from "../../utils/SmallBtn";
 
 export default function ResultPage({ inputValue }) {
    return (
       <main className="w-1/2 px-8 py-4">
-         <div className="flex justify-center items-center">
-            <h1 className="text-white text-xl font-bold">Result</h1>
+         <div className="flex justify-center items-center bg-slate-400 p-4 mx-4 rounded">
+            <div className="flex justify-start items-start w-full">
+               <SmallBtn text="New Meme" />
+            </div>
          </div>
          <ResultContainer inputValue={inputValue} />
       </main>
