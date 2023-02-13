b40afd878db90bd76e6fda665eb6accfed539f55
amankashyap004 <amankashyap004@gmail.com>
2023-02-13 18:27:36 +0530

    fix some bug and add new feature

--- src/pages/public/EditorPage.js
+++ src/pages/public/EditorPage.js
@@ -4,9 +4,9 @@ import DraftEditor from "../../components/public/DraftEditor";
 export default function EditorPage({ inputValue, setInputValue }) {
    return (
       <main className="w-1/2 px-8 py-4">
-         <div className="flex justify-center items-center">
-            <h1 className="text-white text-xl font-bold">Editor</h1>
-         </div>
+         <section className="px-8 py-4">
+            <DraftEditor inputValue={inputValue} setInputValue={setInputValue} />
+         </section>
          <section className="px-8 py-4">
             <DraftEditor inputValue={inputValue} setInputValue={setInputValue} />
          </section>
