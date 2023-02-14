a7597402d38b49a3ea9c2b5fcdca635eb3bdc682
amankashyap004 <amankashyap004@gmail.com>
2023-02-14 13:11:22 +0530

    set image min height and width

--- src/components/public/ResultContainer.js
+++ src/components/public/ResultContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ResultContainer({ inputValue, meme }) {
    return (
-      <section className="flex justify-center items-center flex-col px-2 py-4 rounded relative">
+      <section className="flex justify-center items-center flex-col px-2 py-4 min-w-[300px] min-h-[200px] rounded relative">
          <h3 className="absolute top-0 p-8 font-semibold text-lg">{inputValue}</h3>
          {/* <img src="/images/Image.jpg" className="w-full rounded" alt="post-img" /> */}
          {meme && <img src={meme} alt="random meme" className="w-full rounded " />}
